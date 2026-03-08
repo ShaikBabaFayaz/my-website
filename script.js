2e126a813265752bfeee519ab87d8867dcdd1205
ShaikBabaFayaz <sbfayaz2005@gmail.com>
2026-03-08 11:42:42 +0530

    added script.js for index.html file

---
+++ script.js
@@ -0,0 +1,11 @@
+function showMessage() {
+    alert("Hello Fayaz! Your website is working 🎉");
+}
+
+function changeText() {
+    document.getElementById("title").innerHTML = "You clicked the button!";
+}
+
+function changeColor() {
+    document.body.style.backgroundColor = "lightblue";
+}
